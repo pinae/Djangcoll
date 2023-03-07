40796d78d506316df78c87b3c806d400a23ef525
pinae <pina@pinae.net>
2023-03-07 12:19:12 +0100

    Compile messages for translations (using gettext)

--- docker-setup/init_and_run.sh
+++ docker-setup/init_and_run.sh
@@ -3,6 +3,7 @@ cd /var/www/code
 python3 manage.py wait_for_db
 python3 manage.py migrate || { echo 'migrate failed' ; exit 1; }
 python3 manage.py collectstatic --noinput
+python3 manage.py cmpilemessages
 python3 manage.py initadmin
 cd /home/docker
 /usr/local/bin/uwsgi --ini /etc/uwsgi/apps-enabled/uwsgi-app.ini
