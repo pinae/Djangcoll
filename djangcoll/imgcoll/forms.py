c60becdbd8a010b5b2665e456bb06f5176c7628c
Pina Merkert <pina@pinae.net>
2023-03-17 11:27:06 +0100

    Optional fields in form

--- djangcoll/imgcoll/forms.py
+++ djangcoll/imgcoll/forms.py
@@ -6,7 +6,16 @@ from django.utils.translation import gettext as _
 class CalibrationDataForm(forms.ModelForm):
     email = forms.EmailField(max_length=200, label=_("email"),
                              help_text=_("Enter your email for updates."),
-                             required=True)
+                             required=False)
+    smartphone = forms.CharField(label=_("smartphone"),
+                                 help_text=_("manufacturer and name of your cell phone"),
+                                 required=False)
+    food_name = forms.CharField(label=_("food_name"),
+                                help_text=_("name of the food to analyze"),
+                                required=True)
+    comment = forms.CharField(label=_("comment"),
+                              help_text=_("Let us know how to improve our service!"),
+                              required=False)
 
     class Meta:
         model = CalibrationData
