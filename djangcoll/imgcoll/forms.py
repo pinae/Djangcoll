372984cb02a20c8193da36cb0fb3a029d4154575
Keywan Tonekaboni <me@prometoys.net>
2023-03-20 05:07:37 +0100

    Bilder hinzugefügt, Texte aktualisiert

--- djangcoll/imgcoll/forms.py
+++ djangcoll/imgcoll/forms.py
@@ -5,16 +5,16 @@ from django.utils.translation import gettext as _
 
 class CalibrationDataForm(forms.ModelForm):
     email = forms.EmailField(max_length=200, label=_("email"),
-                             help_text=_("Enter your email for updates."),
+                             help_text=_("Enter your email for updates. (Optional)"),
                              required=False)
     smartphone = forms.CharField(label=_("smartphone"),
-                                 help_text=_("manufacturer and name of your cell phone"),
+                                 help_text=_("Manufacturer and name of your cell phone. (Optional)"),
                                  required=False)
     food_name = forms.CharField(label=_("food_name"),
-                                help_text=_("name of the food to analyze"),
+                                help_text=_("Name of the food to analyze."),
                                 required=True)
     comment = forms.CharField(label=_("comment"),
-                              help_text=_("Let us know how to improve our service!"),
+                              help_text=_("Let us know how to improve our service! (Optional)"),
                               required=False)
 
     class Meta:
--- djangcoll/imgcoll/models.py
+++ djangcoll/imgcoll/models.py
@@ -9,30 +9,30 @@ class CalibrationData(models.Model):
         on_delete=models.CASCADE)
     smartphone = models.CharField(
         max_length=512, verbose_name=_("smartphone"),
-        help_text=_("manufacturer and name of your cell phone"))
+        help_text=_("Manufacturer and name of your cell phone"))
     calibration_image = models.ImageField(
         upload_to='calibration_images',
         verbose_name=_("calibration image"),
-        help_text=_("image of an empty plate for calibration"))
+        help_text=_("Image of an empty plate for calibration"))
     potato_image = models.ImageField(
         upload_to='potato_images',
-        help_text=_("image of a potato slice"))
+        help_text=_("Image of a potato slice"))
     flour_image = models.ImageField(
         upload_to='flour_images',
-        help_text=_("image of some flour"))
+        help_text=_("Image of some flour"))
     milk_image = models.ImageField(
         upload_to='milk_images',
-        help_text=_("image of some milk"))
+        help_text=_("Image of some milk"))
     food_name = models.CharField(
         max_length=512,
-        help_text=_("name of the food to analyze"))
+        help_text=_("Name of the food to analyze"))
     analyze_image = models.ImageField(
         upload_to='analyze_images',
-        help_text=_("image to analyze"))
+        help_text=_("Food image to analyze"))
     comment = models.TextField(
         default="",
         verbose_name=_("comment"),
-        help_text=_("Let us know how to improve our service!"))
+        help_text=_("Let us know how to improve our service! (Optional)"))
 
     def __str__(self):
         return "<%s: %s>" % (self.user.username, self.smartphone)
