2198780334b8babebbf96a84a9cdce597ebe29db
Pina Merkert <pina@pinae.net>
2023-03-22 19:31:23 +0100

    Translation improvements

--- djangcoll/imgcoll/forms.py
+++ djangcoll/imgcoll/forms.py
@@ -4,18 +4,43 @@ from django.utils.translation import gettext as _
 
 
 class CalibrationDataForm(forms.ModelForm):
-    email = forms.EmailField(max_length=200, label=_("email"),
-                             help_text=_("Enter your email for updates. (Optional)"),
-                             required=False)
-    smartphone = forms.CharField(label=_("smartphone"),
-                                 help_text=_("Manufacturer and name of your cell phone. (Optional)"),
-                                 required=False)
-    food_name = forms.CharField(label=_("food_name"),
-                                help_text=_("Name of the food to analyze."),
-                                required=True)
-    comment = forms.CharField(label=_("comment"),
-                              help_text=_("Let us know how to improve our service! (Optional)"),
-                              required=False)
+    email = forms.EmailField(
+        max_length=200,
+        label=_("email"),
+        help_text=_("Enter your email for updates. (Optional)"),
+        required=False)
+    smartphone = forms.CharField(
+        label=_("smartphone"),
+        help_text=_("Manufacturer and name of your cell phone. (Optional)"),
+        required=False)
+    calibration_image = forms.ImageField(
+        label=_("calibration image"),
+        help_text=_("Image of an empty plate for calibration"),
+        required=True)
+    potato_image = forms.ImageField(
+        label=_("potato image"),
+        help_text=_("Image of a potato slice"),
+        required=True)
+    flour_image = forms.ImageField(
+        label=_("flour image"),
+        help_text=_("Image of some flour"),
+        required=True)
+    milk_image = forms.ImageField(
+        label=_("milk image"),
+        help_text=_("Image of some milk"),
+        required=True)
+    food_name = forms.CharField(
+        label=_("food name"),
+        help_text=_("Name of the food to analyze"),
+        required=True)
+    analyze_image = forms.ImageField(
+        label=_("food image"),
+        help_text=_("Food image to analyze"),
+        required=True)
+    comment = forms.CharField(
+        label=_("comment"),
+        help_text=_("Let us know how to improve our service! (Optional)"),
+        required=False)
 
     class Meta:
         model = CalibrationData
